d1df22c9b70cb20cb5ba00011c799acce4d8c6b7
Patrick Prell <patrick.prell@droneshield.com>
2026-01-12 18:49:45 +1100

    changes

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,20 @@ use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    let gpio_pin30_config: *mut u32 = 0x50_000_778 as *mut u32;
+    let gpio_pin17_config: *mut u32 = 0x50_000_744 as *mut u32;
     let dir_config_pos: u32 = 0;
-    let pin30_config: u32 = 1 << dir_config_pos;
+    let pin17_config: u32 = 1 << dir_config_pos;
     unsafe {
-        write_volatile(gpio_pin30_config, pin30_config);
+        write_volatile(gpio_pin17_config, pin17_config);
     }
     let gpio_out_addr: *mut u32 = 0x50_000_504 as *mut u32;
-    let pin30_out_pos: u32 = 30;
+    let pin17_out_pos: u32 = 17;
     let mut pin_state: bool = true;
     loop {
         unsafe {
-            write_volatile(gpio_out_addr, (pin_state as u32) << pin30_out_pos);
+            write_volatile(gpio_out_addr, (pin_state as u32) << pin17_out_pos);
         }
-        for _ in 0..400_000 {
+        for _ in 0..40_00_000 {
             nop();
         }
         pin_state = !pin_state
